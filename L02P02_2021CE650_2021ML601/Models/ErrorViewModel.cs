b0133c890d3b956e8b39512c879b3f85001f541c
Fracisco Chacon <Fracisco Chacon@DESKTOP-6G7DPU6>
2024-04-20 08:43:00 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2021CE650_2021ML601/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021CE650_2021ML601.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
