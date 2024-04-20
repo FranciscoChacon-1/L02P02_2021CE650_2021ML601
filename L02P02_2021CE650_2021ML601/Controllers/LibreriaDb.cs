eb0fda423bf186d6db45396aed973ecb974e3c87
Fracisco Chacon <Fracisco Chacon@DESKTOP-6G7DPU6>
2024-04-20 08:55:33 -0600

    Agregue razor

---
+++ L02P02_2021CE650_2021ML601/Controllers/LibreriaDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace L02P02_2021CE650_2021ML601.Controllers
+{
+    public class LibreriaDb : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
