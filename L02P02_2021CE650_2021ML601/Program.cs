658ccd56fb3604715bffe9b52b5ec96bcbdf0b5e
Fracisco Chacon <Fracisco Chacon@DESKTOP-6G7DPU6>
2024-04-20 09:40:32 -0600

    cambios

--- L02P02_2021CE650_2021ML601/Program.cs
+++ L02P02_2021CE650_2021ML601/Program.cs
@@ -6,11 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+
 
 builder.Services.AddDbContext<LibreriaDbContext>(options => options.UseSqlServer
-(builder.Configuration.GetConnectionString("LibreriaDbConnection"))
+(builder.Configuration.GetConnectionString("LibreriaDbconnection"))
 );
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
